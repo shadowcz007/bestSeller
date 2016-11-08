a5aa0889ef9e9c249f8a0e6396ca9c78c3292857
shadow <389570357@qq.com>
2016-11-08 11:17:23 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 
-const electron = require('electron')
-const ipcMain = require('electron').ipcMain
-
+const electron = require('electron');
+const ipcMain = require('electron').ipcMain;
 const path=require('path');
-const fs = require('fs-extra');
-
 const bestdb=require('./app/js/bestdb.js');
-
 ipcMain._maxListeners=200;
-//console.log(ipcMain._maxListeners)
 
 global.sharedObj = {myvar: "hellofrommainjs",
                     result:"1",
@@ -35,14 +30,9 @@ global.sharedObj = {myvar: "hellofrommainjs",
                       },
                       count:"",
                       productWinUrls:""
-
                     };
- //selected:["name",0]
- //0,1,2,3,4
- //0--mainbtn,1--dpf,2--dpc,3-dpcc
-// Module to control application life.
+
 const app = electron.app
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 /////////////////////////////////
@@ -67,95 +57,90 @@ let mainWin,departmentWin,bestSellersWin,stockWin,productWin,topReviewersWin;
 
 function createWindow () {
   createMainWin();
+  SpiderWin.init();
 }
 function createMainWin () {
-
   mainWin = new BrowserWindow({
     frame:true,
     resizable: false,
-    //alwaysOnTop:true,
     title:'BestSellers',
     titleBarStyle:'hidden-inset',
-   // fullscreen:true,
-
     width: 1280,
     height: 800,
-    'min-width': 500,
-    'min-height': 500,
     'accept-first-mouse': true,
     webPreferences: {
         experimentalFeatures:true,
         experimentalCanvasFeatures:true,
         plugins: true,
         nodeIntegration: true,//这句是使用node 模块
-
       }
-  })
-
-  mainWin.loadURL(`file://${__dirname}/app/index.html`)
-  mainWin.webContents.openDevTools()
+  });
+  mainWin.loadURL(`file://${__dirname}/app/index.html`);
   mainWin.on('closed', function () {
     mainWin = null
   })
 }
 
+var option={
+           show:false,
+            backgroundColor:'#66CD00',
+            frame: false,
+            resizable: true,
+            alwaysOnTop:true,
+            x:100,
+            y:100,
+            width: 40,
+            height: 40,
+            webPreferences: {
+                experimentalFeatures:true,
+                plugins: true,
+                nodeIntegration: true,//这句是使用node 模块
+              }
+            };
+            var option2={
+                        backgroundColor:'#ffffff',
+                        frame: false,
+                        resizable: true,
+                        alwaysOnTop:true,
+                        movable:true,
+                        x:20,
+                        y:800,
+                        width: 500,
+                        height: 60,
+                        webPreferences: {
+                            experimentalFeatures:true,
+                            plugins: true,
+                            nodeIntegration: true,//这句是使用node 模块
+                          }
+                        };
 
 function createBestSellersWin () {
-  bestSellersWin = new BrowserWindow({
-    frame:true,
-    resizable: true,
-    alwaysOnTop:true,
-    title:'bestSellers',
-    x:1,
-    y:1,
-    titleBarStyle:'hidden-inset',
-    closable:true,
-    movable:true,
-    width: 100,
-    height: 100,
-    webPreferences: {
-        experimentalFeatures:true,
-        plugins: true,
-        nodeIntegration: true,//这句是使用node 模块
-      }
-  })
-
+  bestSellersWin = new BrowserWindow(option)
   bestSellersWin.loadURL(`file://${__dirname}/app/tpl/rank.html`)
-
-  bestSellersWin.webContents.openDevTools()
-
   bestSellersWin.on('closed', function () {
     bestSellersWin = null
   })
 }
 
+
+
+var SpiderWin ={
+    init:function(){
+          this.win= new BrowserWindow(option2);
+          this.win.loadURL(`file://${__dirname}/app/tpl/spiderWin.html`);
+          this.win.on('closed', function () {
+            this.win = null
+          });
+         }
+
+}
+
 //
 
+
 function createDepartmentWin () {
-  departmentWin = new BrowserWindow({
-    frame:true,
-    resizable: true,
-    alwaysOnTop:true,
-    title:'bestSellers',
-    x:1,
-    y:1,
-    titleBarStyle:'hidden-inset',
-    closable:true,
-    movable:true,
-    width: 100,
-    height: 100,
-    webPreferences: {
-        experimentalFeatures:true,
-        plugins: true,
-        nodeIntegration: true,//这句是使用node 模块
-      }
-  })
-  //bestSellersWin.loadURL('https://www.amazon.com/Best-Sellers-Appliances/zgbs/appliances/ref=zg_bs_nav_0')
-
+  departmentWin = new BrowserWindow(option)
   departmentWin.loadURL(`file://${__dirname}/app/tpl/department.html`)
-
-  departmentWin.webContents.openDevTools()
-
   departmentWin.on('closed', function () {
     departmentWin = null
   })
@@ -166,6 +151,7 @@ function createDepartmentWin () {
 function createStockWindow () {
   // Create the browser window.
   stockWin = new BrowserWindow({
+    show:false,
     frame:false,
     resizable: true,
     alwaysOnTop:false,
@@ -184,7 +170,7 @@ function createStockWindow () {
       }
   })
   stockWin.loadURL(`file://${__dirname}/app/tpl/stock.html`)
-  stockWin.webContents.openDevTools()
+//  stockWin.webContents.openDevTools()
   stockWin.on('closed', function () {
     stockWin = null
   })
@@ -193,25 +179,9 @@ function createStockWindow () {
 /////look productWin
 
 function createProductWindow () {
-  productWin = new BrowserWindow({
-    //frame:false,
-    resizable: true,
-    alwaysOnTop:false,
-    title:'product',
-    titleBarStyle:'hidden-inset',
-    width: 400,
-    height: 400,
-    x:0,
-    y:0,
-    center:false,
-    webPreferences: {
-        experimentalFeatures:true,
-        plugins: true,
-        nodeIntegration: true//这句是使用node 模块
-      }
-  });
+  productWin = new BrowserWindow(option);
   productWin.loadURL(`file://${__dirname}/app/tpl/product.html`)
-  productWin.webContents.openDevTools();
+  //productWin.webContents.openDevTools();
   productWin.on('closed', function () {
     productWin = null
   });
@@ -221,25 +191,9 @@ function createProductWindow () {
 /////topReviewersWin
 
 function createTopReviewersWindow () {
-  topReviewersWin = new BrowserWindow({
-    //frame:false,
-    resizable: true,
-    alwaysOnTop:false,
-    title:'topReviewers',
-    titleBarStyle:'hidden-inset',
-    width: 400,
-    height: 400,
-    x:0,
-    y:0,
-    center:false,
-    webPreferences: {
-        experimentalFeatures:true,
-        plugins: true,
-        nodeIntegration: true//这句是使用node 模块
-      }
-  });
+  topReviewersWin = new BrowserWindow(option);
   topReviewersWin.loadURL(`file://${__dirname}/app/tpl/topReviewers.html`)
-  topReviewersWin.webContents.openDevTools();
+  //topReviewersWin.webContents.openDevTools();
   topReviewersWin.on('closed', function () {
     topReviewersWin = null
   });
@@ -314,18 +268,6 @@ ipcMain.on('click-button', function (event, arg) {
 
 })
 
-ipcMain.on('asynchronous-message', function (event, arg) {
-  console.log(arg)  // prints "ping"
-
-  event.sender.send('asynchronous-reply', '-----main-sender-send----pong---------')
-})
-
-ipcMain.on('synchronous-message', function (event, arg) {
-  console.log(arg)  // prints "ping"
-  event.returnValue = 'main-returnValue-----pong---------'
-})
-
-
 ipcMain.on('catch_rankStart',function (event, arg) {
   console.log(arg);
   if (bestSellersWin==null) {
@@ -334,18 +276,16 @@ ipcMain.on('catch_rankStart',function (event, arg) {
     bestSellersWin.reload();
   };
     global.sharedObj.rankStart=arg;
+    SpiderWin.win.webContents.send('spiderWin', '爬取产品数量'+arg[0].length*100)
 });
 
 ipcMain.on('result',function (event, arg) {
-
   console.log('result~~~~~~~~~~~~~'+arg);
-
+  SpiderWin.win.webContents.send('spiderWin', 'result~~~~~~~~~~~~~'+arg)
 });
 
 
-
 ipcMain.on('catch-result-save',function (event, arg) {
- //mainWin.reload();
   //console.log("catch-result-save----------from-webview"+JSON.stringify(arg[1]).replace(/\\n/g,''));
   //console.log(path.join(`${__dirname}`,'app/js/data/test1.json'))
   let index=arg[0].replace(/.*\/Best-Sellers-|\/zgbs.*/ig,'')+'/'+global.sharedObj.count+"/"+arg[0].replace(/.*pg=|&ajax.*=/ig,'');
@@ -356,23 +296,16 @@ ipcMain.on('catch-result-save',function (event, arg) {
   for (var i = json.length - 1; i >= 0; i--) {
     json[i].title=json[i].title.replace(/.*.com\/|\/dp.*/ig,'');
   };
-  fs.outputJson(path.join(`${__dirname}`,'app/js/data/'+index.toLowerCase()+'.json'),json);
-  console.log("fsjout --------json------------from webview--"+path.join(`${__dirname}`,'app/js/data/'+index.toLowerCase()+'.json'))
-
   bestdb.update(DP,counts,json);
-
-  //event.sender.send('catch-result-save-reply',[index,bs]);
-
 })
 
 ipcMain.on('catch_stock',function (event, arg) {
-  console.log(arg);
+//  console.log(arg);
    global.sharedObj.stockURL=arg;
   if (stockWin==null) {
     createStockWindow();
   }else{
     stockWin.reload();
-
   };
 
   event.sender.send('catch_stock_result',arg);
@@ -382,11 +315,7 @@ ipcMain.on('catch_stock',function (event, arg) {
 ipcMain.on('catch_stock-result-save',function (event, arg) {
   let time=new Date();
   let fileName=time.getMonth()+"m"+time.getDate()+"d"+time.getHours()+'h'+time.getMinutes()+'min';
-
-  fs.outputJson(path.join(`${__dirname}`,'app/js/data/stock/'+fileName+'.json'),arg);
-
-  console.log("fsjout --------json------------from webview--"+path.join(`${__dirname}`,'app/js/data/stock/'+fileName+'.json'))
-
+  //fs.outputJson(path.join(`${__dirname}`,'app/js/data/stock/'+fileName+'.json'),arg);
   event.sender.send('catch_stock-result-save-reply',path.join(`${__dirname}`,'app/js/data/stock/'+fileName+'.json'));
 
 })
@@ -398,27 +327,26 @@ ipcMain.on('catch_lookProduct',function (event, args) {
     createProductWindow();
   }else{
     productWin.reload();
-
   };
-
-  //event.sender.send('catch_lookProduct_result',args);
+  SpiderWin.win.webContents.send('spiderWin', '爬取产品数量'+args[0].length)
 
 });
 
 ipcMain.on('catch_topReviewers',function (event, args) {
-  console.log(args);
+  //console.log(args);
   global.sharedObj.topReviewersUrl=args;
   global.sharedObj.topReviewers=args[3];
   if (topReviewersWin==null) {
     createTopReviewersWindow();
   }else{
     topReviewersWin.reload();
-
   };
+  SpiderWin.win.webContents.send('spiderWin', '爬取数量'+args[0].length)
+
 });
 
 ipcMain.on('catch',function (event, arg) {
-  console.log(arg);
+  //console.log(arg);
   if (departmentWin==null) {
     createDepartmentWin();
   }else{
